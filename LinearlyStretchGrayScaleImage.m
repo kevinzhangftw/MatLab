ce281380ffe8a1fee596f68a5386514c00fa8e9d
Kai Zhang <kai@d207-023-223-020.wireless.sfu.ca>
2017-01-12 14:18:55 -0800

    initial commit

---
+++ LinearlyStretchGrayScaleImage.m
@@ -0,0 +1,18 @@
+% read image
+im = imread('dark.tif');
+
+% show image
+figure,imshow(im);
+
+i = im(:,:,1);
+
+rtemp = min(i);         % find the min. value of pixels in all the columns (row vector)
+rmin = min(rtemp);      % find the min. value of pixel in the image
+rtemp = max(i);         % find the max. value of pixels in all the columns (row vector)
+rmax = max(rtemp);      % find the max. value of pixel in the image
+
+m = 255/(rmax - rmin);  % find the slope of line joining point (0,255) to (rmin,rmax)
+c = 255 - m*rmax;       % find the intercept of the straight line with the axis
+
+i_new = m*i + 50;        % transform the image according to new slope
+figure,imshow(i_new);
